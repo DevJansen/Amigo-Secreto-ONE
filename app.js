99acc42e054fa834ba6766dc252d2320555ac60a
Jhonata Jansen <codejhonatajansen@gmail.com>
2025-07-25 14:47:20 -0300

    criado a função de adicionar o nome do amigo na lista

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 
+let listaDeNomes = [];
 
+function adicionarAmigo() {
+  
+  const inputNome = document.getElementById('amigo');
+  const novoNome = inputNome.value.trim();
+
+  if (novoNome !== '') {
+
+    listaDeNomes.push(novoNome);
+
+    inputNome.value = '';
+
+    atualizarListaNaTela();
+  } else {
+    alert('Por favor, digite um nome antes de adicionar!');
+  }
+  
+}
 
 
  
